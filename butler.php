bc8320b9d25cf26eda599b0f8fccddadda57a519
Sean Wu <sean@seanwuapps.com>
2019-02-21 16:09:49 +1100

    add package version

--- butler.php
+++ butler.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Australia/Sydney');
 define('BUTLER_DIR', __DIR__);
-define('BUTLER_VER', '1.01a');
+define('BUTLER_VER', '1.01a - @package_version@');
 
 require_once BUTLER_DIR . '/vendor/autoload.php';
 
